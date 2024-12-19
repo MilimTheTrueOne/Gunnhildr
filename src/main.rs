800758aeae1c07c3149223017d20f46e330ea13c
Morrígan <81323548+TheMorrigna@users.noreply.github.com>
2024-12-19 10:25:58 +0100

    basic setup and dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpServer};
+use anyhow::Ok;
+
+#[actix_web::main]
+async fn main() -> anyhow::Result<()> {
+    HttpServer::new(|| App::new().service(greet))
+        .bind(("127.0.0.1", 5678))?
+        .run()
+        .await?;
+    Ok(())
+}
+
+#[get("/")]
+async fn greet() -> actix_web::Result<String> {
+    todo!()
 }
