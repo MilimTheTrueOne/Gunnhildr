553da4e23c1de62607f76058eb51653c1d00b697
Morrígan <phantom.morrigan@proton.me>
2024-12-25 16:02:39 +0100

    setup logging

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use actix_web::{get, web, App, HttpServer};
+use log::info;
 use migration::MigratorTrait;
 use sea_orm::Database;
 
@@ -6,6 +7,10 @@ mod entity;
 
 #[actix_web::main]
 async fn main() -> anyhow::Result<()> {
+    env_logger::builder()
+        .filter_level(log::LevelFilter::Info)
+        .init();
+
     let conn = Database::connect("sqlite:file:gunnhildr.db").await.unwrap();
     migration::Migrator::up(&conn, None).await.unwrap();
 
@@ -13,6 +18,8 @@ async fn main() -> anyhow::Result<()> {
         .bind(("127.0.0.1", 5678))?
         .run()
         .await?;
+
+    info!("Server shut down! Goodbye!");
     Ok(())
 }
 
